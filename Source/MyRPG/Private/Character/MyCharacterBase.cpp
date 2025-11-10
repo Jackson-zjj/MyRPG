927552c68de1240924f107f2cf85bc723004698b
Jackson <347960869@qq.com>
2025-11-10 17:18:28 +0800

    角色设置骨骼体并添加武器

--- Source/MyRPG/Private/Character/MyCharacterBase.cpp
+++ Source/MyRPG/Private/Character/MyCharacterBase.cpp
@@ -4,6 +4,9 @@
 AMyCharacterBase::AMyCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::Type::NoCollision);
 }
 
 void AMyCharacterBase::BeginPlay()
--- Source/MyRPG/Public/Character/MyCharacterBase.h
+++ Source/MyRPG/Public/Character/MyCharacterBase.h
@@ -15,5 +15,7 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
-	
+
+	UPROPERTY(EditAnywhere, Category = "combat")
+	TObjectPtr<USkeletalMeshComponent> Weapon;
 };
